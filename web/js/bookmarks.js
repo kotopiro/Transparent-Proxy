44c4d3c05d51b340208ee4c3b701dc470e98e499
kotopiro <mnxsv69789@yahoo.co.jp>
2025-12-05 21:02:24 +0900

    Create bookmarks.js

---
+++ web/js/bookmarks.js
@@ -0,0 +1,24 @@
+export class Bookmarks {
+  constructor(listEl){
+    this.listEl=listEl;
+    this.items=JSON.parse(localStorage.getItem('tp_bookmarks')||'[]');
+    this.render();
+  }
+  add(title,url){
+    this.items.unshift({title,url});
+    localStorage.setItem('tp_bookmarks',JSON.stringify(this.items));
+    this.render();
+  }
+  render(){
+    this.listEl.innerHTML='';
+    for(const b of this.items){
+      const li=document.createElement('li');
+      const a=document.createElement('a');
+      a.href='#';
+      a.textContent=b.title;
+      a.addEventListener('click',e=>{e.preventDefault();window.open(b.url,'_blank');});
+      li.appendChild(a);
+      this.listEl.appendChild(li);
+    }
+  }
+}
