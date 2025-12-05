d5e13f7787df739c7d753ce530f5c421dd0f7289
kotopiro <mnxsv69789@yahoo.co.jp>
2025-12-05 20:20:35 +0900

    Delete server directory

--- server/uv.middleware.js
+++
@@ -1,9 +0,0 @@
-import { createBareServer } from "uvod/bare";
-import { createServer } from "uvod/server";
-
-const bare = createBareServer();
-
-export function UVMiddleware(req, res, next) {
-  if (bare.routeRequest(req, res)) return;
-  next();
-}
