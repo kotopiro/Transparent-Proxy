c9a8b83d9db09ecc2b5a1ccfe845fe9b7028263b
kotopiro <mnxsv69789@yahoo.co.jp>
2025-12-05 20:56:36 +0900

    Delete scripts directory

--- scripts/deploy-workers.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cd worker
-if ! command -v npx >/dev/null 2>&1; then
-  echo "Please install Node.js & npm"
-  exit 1
-fi
-npx wrangler publish
--- scripts/make-zip.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-ZIPNAME="transparent-proxy-$(date +%Y%m%d-%H%M).zip"
-zip -r ${ZIPNAME} web worker docker scripts README.md LICENSE -x "*.git/*" "*.DS_Store"
-echo "Created ${ZIPNAME}"
